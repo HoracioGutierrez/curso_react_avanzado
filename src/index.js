3ee2680038f57bf8f123d839676cef2df4d83079
HoracioGutierrez <horacio.estevez@gmail.com>
2020-08-20 12:20:39 -0300

    Primera implementacion de context y hooks

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import App from './ui/layout/App'
 import {Provider} from "react-redux"
 import store from "./api/store"
 import {BrowserRouter} from "react-router-dom"
+//import {ProviderContext} from "./api/context"
 
 ReactDOM.render(
     <BrowserRouter>
         <Provider store={store}>
-            <App/>
+            {/* <ProviderContext value={{x:1}}> */}
+                <App/>
+            {/* </ProviderContext> */}
         </Provider>
     </BrowserRouter>,
     document.getElementById("root")
--- src/ui/layout/Footer.js
+++ src/ui/layout/Footer.js
@@ -1,4 +1,39 @@
-import React from 'react'
+import React , {useContext} from 'react'
+//import context , {Consumer} from "../../api/context"
+
+/* class Footer extends React.Component {
+
+    static contextType = context
+
+    componentDidMount(){
+        console.log(this.context)
+    }
+    
+    render(){
+        console.log(this.context)
+        return(
+            <footer>
+                <p>&copy; Copyright 2020</p>
+            </footer>
+        )
+    }
+}
+ */
+
+/* const Footer = () => {
+
+    let contexto = useContext(context)
+
+    function cambiarX(){
+        contexto.x = "nuevo valor"
+    }
+
+    return (
+        <footer>
+            <p>&copy; Copyright 2020</p>
+        </footer>
+    )
+} */
 
 const Footer = () => {
     return (
